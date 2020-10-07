624f603ad536b2aef7fc5cdfd9780799483c389d
Goldengide <amowogbajegideon@gmail.com>
2020-10-07 12:40:29 +0100

    module 4

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}))
 
 const userData = fs.readFileSync("src/json/users.json", "utf-8");
 const users = JSON.parse(userData);
@@ -20,5 +21,19 @@ app.get('/savings', (req,res) => res.render('account', {account: accounts.saving
 app.get('/checking', (req,res) => res.render('account', {account: accounts.checking}));
 app.get('/credit', (req,res) => res.render('account', {account: accounts.credit}));
 app.get('/profile', (req,res) => res.render('profile', {user: users[0]}));
-
+app.get('/transfer', (req,res) => res.render('transfer'));
+app.post('/transfer', (req,res) => {
+    accounts[req.body.from].balance = parseInt(accounts[req.body.from].balance) - parseInt(req.body.amount)
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, "json/accounts.json"), accountsJSON, "utf-8"); 
+    res.render('transfer', {message: 'Transfer Completed'});
+});
+app.get('/payment', (req,res) => { res.render('payment', {account: accounts.credit})});
+app.post('/payment', (req,res) => {
+    accounts.credit.balance = parseInt(accounts.credit.balance) - parseInt(req.body.amount);
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, "json/accounts.json"), accountsJSON, "utf-8");
+    res.render('payment', { message: "Payment Successful", account: accounts.credit });
+    
+})
 app.listen(3000, () => console.log("Hello"));
