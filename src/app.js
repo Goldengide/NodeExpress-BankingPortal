65cbdb5fc13366752f8bdab5050e49ab4ae1243b
Goldengide <amowogbajegideon@gmail.com>
2020-10-07 13:48:47 +0100

    module 5 correction 1

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const express = require('express');
 
-const { writeJSON } = require('./data');
+const { accounts, users, writeJSON } = require('./data');
 
 const app = express();
 
